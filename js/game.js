5dd2f871709675ba0dc103e95d53de9da227aa45
ddavison <daniel.jj.davison@gmail.com>
2014-01-11 16:30:30 -0500

    adding sound file; multiple fixes

--- js/game.js
+++ js/game.js
@@ -5,8 +5,11 @@
 var LOC_DOT = "dot";
 var LOC_SCORE = "score";
 var LOC_CONTAINER = "mainContainer";
+var LOC_MULTIPLIER = "multiplier";
+var LOC_POPSOUND = "pop";
+var LOC_GETREADYMSG = "getReadyMessage";
 
-var DEFAULT_TIMEOUT = 5;
+var DEFAULT_TIMEOUT = 5000;
 
 var Game = function() {
   this.debugging = true;
@@ -14,33 +17,91 @@ var Game = function() {
 
   this.version = "1.0.0";
 
-  this.score = 0;
-  this.clickScore = 1;
+  /**
+   * The player's score
+   * @type {number}
+   */
+  this.score = 1;
 
+  /**
+   * The current multiplier.
+   * @type {number}
+   */
   this.multiplier = 1;
+
+  /**
+   * The <div id="mainContainer"> element that contains the dot.
+   * @type {HTMLElement}
+   */
+  this.container = null;
+
+  this.settings = null;
 
   var calculateTimeout = function() {
     return DEFAULT_TIMEOUT; // later, it should calculate the score and reduce the time based on it.
-  }
+  };
 
-  var timeout = calculateTimeout();
+  var timer = null;
 
   /**
    * Initializer
    * This can load in the cookie, too.
    */
   this.init = function() {
-    this.newDot();
-  }
+    // configure the settings.
+    // start the timer.
+    this.settings = localStorage['settings'] ? localStorage['settings'] : {
+      // set default settings.
+      playSounds: true
+    };
+
+    this.container = this.find(LOC_CONTAINER);
+
+    restartTimer();
+  };
+
+  var set = function(obj, value) {
+    // use the id as the setting name.
+    localStorage[obj] = value;
+  };
+
+  /**
+   * Start / Restart the timer.
+   */
+  var restartTimer = function() {
+    clearInterval(timer);
+
+    timer = setInterval(function() {
+      if (game.debugging)
+        console.log("Interval hit: resetting multiplier to x1");
+
+        game.multiplier = 1; // reset the multiplier.
+        game.newDot();
+        game.update();
+
+    }, calculateTimeout());
+  };
+
+  /**
+   * Pause the game.
+   */
+  this.pause = function() {
+    clearInterval(timer);
+
+  };
 
   /**
    * Function that executes when it's clicked.
    */
   var clicked = function() {
     // they clicked on appearing dot.
+
     var scoreWas = game.score;
 
-    game.score += (game.clickScore * game.multiplier);
+    // base the score on how small the dot is.
+
+    game.score += (100 * game.multiplier);
+
     game.multiplier += 1;
 
     if (game.debugging)
@@ -51,12 +112,25 @@ var Game = function() {
         game.multiplier
       );
 
+    // reset the timer
+
+    restartTimer();
+
     game.update();
-  }
+    game.newDot();
+  };
+
+  /*var playPopSound = function() {
+    if (game.settings.playSounds) {
+      var sound = game.find(LOC_POPSOUND);
+      sound.currentTime = 0;
+      sound.play();
+    }
+  };*/
 
   var getRandomNumber = function(to) {
     return Math.floor((Math.random() * to)+1);
-  }
+  };
 
   this.newDot = function() {
     if (this.debugging)
@@ -65,7 +139,7 @@ var Game = function() {
     var existingElement = this.find(LOC_DOT);
 
     if (existingElement != null)
-      this.find(LOC_CONTAINER).removeChild(existingElement);
+      this.container.removeChild(existingElement);
 
     var element = document.createElement('div');
     element.id = 'dot';
@@ -75,19 +149,25 @@ var Game = function() {
     var height = width; // keep it proportional.
 
     // location properties.
-    var x = getRandomNumber(screen.width);
-    var y = getRandomNumber(screen.height);
+    var x = getRandomNumber(window.innerWidth);
+    var y = getRandomNumber(window.innerHeight);
     element.style.backgroundColor = '#fff';
     element.style.position = 'absolute';
     element.style.borderRadius = '50%';
     element.style.mozBorderRadius = '50%';
     element.style.webkitBorderRadius = '50%';
 
-    this.find(LOC_CONTAINER).appendChild(element);
+    // before we append the child, we should ensure that it's within the bounds of the screen.
+    if ((x + width > window.innerWidth) || (y + height > window.innerHeight)) {
+      element = null;
+      this.newDot();
+    }
 
-    if (element.addEventListener)
-      element.addEventListener('click', clicked, false);
-    else if (element.attachEvent)
+    this.container.appendChild(element);
+
+      if (element.addEventListener)
+        element.addEventListener('click', clicked, false);
+      else if (element.attachEvent)
       element.attachEvent('click', clicked);
 
     element.style.width = width + "px";
@@ -102,16 +182,16 @@ var Game = function() {
       );
 
     return element;
-  }
+  };
 
   /**
    * Game update function.
    * This function will update all UI elements and stuff.
    */
   this.update = function() {
-    this.find(LOC_SCORE).innerText = this.score;
-    this.newDot();
-  }
+    this.find(LOC_SCORE).innerText = ""+this.score;
+    this.find(LOC_MULTIPLIER).innerText = "x" + this.multiplier;
+  };
 
   /**
    * Since we will only be using ID's, we will use game.find() to find elements with id.
@@ -121,5 +201,5 @@ var Game = function() {
   this.find = function(id) {
     if (id != null)
       return document.getElementById(id);
-  }
+  };
 };
