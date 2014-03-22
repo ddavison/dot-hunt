21036d12922de7a0380499c5123ebccef8248320
ddavison <daniel.jj.davison@gmail.com>
2014-03-22 02:01:23 -0400

    fix

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-export PATH=/usr/local/bin:/Users/ddavison/.rvm/rubies/ruby-2.1.0/bin::$PATH
+export PATH=/usr/local/bin:/Users/ddavison/.rvm/rubies/ruby-2.1.0/bin:/usr/local/share/npm/bin:$PATH
 
 grunt jenkins --no-color
