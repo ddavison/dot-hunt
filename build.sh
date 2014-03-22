65e507c236d09a818b9bbd7334bcd9a7d6459d87
ddavison <daniel.jj.davison@gmail.com>
2014-03-22 02:19:51 -0400

    build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 export PATH=/usr/local/bin:/Users/ddavison/.rvm/rubies/ruby-2.1.0/bin:/usr/local/share/npm/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0@global/bin:$PATH
-
+bundle install
 grunt --no-color
