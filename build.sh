632b4dfedd66a67ac7f9ff90f5ce694695d63663
ddavison <daniel.jj.davison@gmail.com>
2014-03-22 02:28:23 -0400

    this?

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-export PATH=/usr/local/bin:/Users/ddavison/.rvm/rubies/ruby-2.1.0/bin:/usr/local/share/npm/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0@global/bin:$PATH
-alias haml="/Users/ddavison/.rvm/gems/ruby-2.1.0/bin/haml"
-alias coffee="/usr/local/bin/coffee"
-alias compass="/Users/ddavison/.rvm/gems/ruby-2.1.0/bin/compass"
+export PATH=/usr/local/bin:/Users/ddavison/.rvm/rubies/ruby-2.1.0/bin:/usr/local/share/npm/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0@global/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin/haml:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin/coffee:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin/compass:$PATH
 
 grunt --no-color
