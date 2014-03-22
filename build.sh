c9beec2bf97134eefafb3b255e660c90ffd7a3e1
ddavison <daniel.jj.davison@gmail.com>
2014-03-22 02:26:33 -0400

    trying this

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 export PATH=/usr/local/bin:/Users/ddavison/.rvm/rubies/ruby-2.1.0/bin:/usr/local/share/npm/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0@global/bin:$PATH
-bundle install
+alias haml="/Users/ddavison/.rvm/gems/ruby-2.1.0/bin/haml"
+alias coffee="/usr/local/bin/coffee"
+alias compass="/Users/ddavison/.rvm/gems/ruby-2.1.0/bin/compass"
+
 grunt --no-color
