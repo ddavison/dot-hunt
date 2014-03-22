e13ff0d6502d00cc66bc34b3f1fdef7936fff39f
ddavison <daniel.jj.davison@gmail.com>
2014-03-22 02:06:51 -0400

    final fix?

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-export PATH=/usr/local/bin:/Users/ddavison/.rvm/rubies/ruby-2.1.0/bin:/usr/local/share/npm/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0:/Users/ddavison/.rvm/gems/ruby-2.1.0@global:$PATH
+export PATH=/usr/local/bin:/Users/ddavison/.rvm/rubies/ruby-2.1.0/bin:/usr/local/share/npm/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0@global/bin:$PATH
 
 grunt --no-color
