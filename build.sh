b434d789115bf282c8b8aba4c3036a7187b2efdd
ddavison <daniel.jj.davison@gmail.com>
2014-03-23 17:17:05 -0400

    fixing gitignore, adding travis

--- build.sh
+++
@@ -1,3 +0,0 @@
-export PATH=/usr/local/bin:/Users/ddavison/.rvm/rubies/ruby-2.1.0/bin:/usr/local/share/npm/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0@global/bin:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin/haml:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin/coffee:/Users/ddavison/.rvm/gems/ruby-2.1.0/bin/compass:$PATH
-
-grunt --no-color
